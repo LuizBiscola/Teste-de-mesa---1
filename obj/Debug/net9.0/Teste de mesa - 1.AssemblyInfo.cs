03465e31056b0d321c8665c322dc44d0e137ac84
Luiz Eduardo <163674230+LuizBiscola@users.noreply.github.com>
2025-04-02 00:40:02 -0300

    catalogo

--- obj/Debug/net9.0/Teste de mesa - 1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Teste de mesa - 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Teste de mesa - 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+208d92701f9dd5ae6dd3ce131193cc25ae7b0c44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Teste de mesa - 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Teste de mesa - 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
